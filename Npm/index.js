701bfb85c2a299e89b80c683f8097555ebe3c033
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-10-14 08:05:31 +0530

    add chalk and nodemon

---
+++ Npm/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+const warning = chalk.hex('#FFA500');
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red('Hello world!'));
+console.log(warning('Warning!'));
